6f89b6fee6c7a02771f0c12b953e9db2d408a945
kevin collier <collier.kevin.m@gmail.com>
2023-11-10 22:32:04 +0900

    completed devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # devise_for :users
-  # root to: "pages#home"
-
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
-      resources :users
-      resources :tags
-      resources :user_answers
-      resources :learning_points do
-        resources :questions do
-          resources :answers
-        end
-      end
-    end
-  end
+  root to: "pages#home"
 end
