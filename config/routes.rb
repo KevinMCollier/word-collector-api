1a065c58fbd755eaeeb6651ab6fc52e1b6d35612
kevin collier <collier.kevin.m@gmail.com>
2023-11-10 20:51:04 +0900

    create api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # devise_for :users
+  # root to: "pages#home"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+      resources :users
+      resources :tags
+      resources :user_answers
+      resources :learning_points do
+        resources :questions do
+          resources :answers
+        end
+      end
+    end
+  end
 end
