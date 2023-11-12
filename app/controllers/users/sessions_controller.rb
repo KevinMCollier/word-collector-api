bdb567a44f8a97afc2faf1944601228676275c06
kevin collier <collier.kevin.m@gmail.com>
2023-11-12 22:32:19 +0900

    updated cors origins

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,11 @@ module Users
     private
 
     def respond_with(resource, _opts = {})
-      render json: resource
+      if resource.persisted?
+        render json: { user: resource, token: resource.authentication_token }
+      else
+        render json: { error: 'Invalid email or password'}, status: :unauthorized
+      end
     end
 
     def respond_to_on_destroy
