1bc6149b285ae537dee303d9c5ef3580702c1ad2
kevin collier <collier.kevin.m@gmail.com>
2023-12-08 20:43:48 +0900

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,8 @@ Flashcard.destroy_all
 Tag.destroy_all
 User.destroy_all
 
-# Users
-users_data = [
-  { email: 'santa@northpole.com', username: 'SantaClaus', password: 'HoHoHo123' },
-  { email: 'rudolph@reindeers.com', username: 'RudolphRedNose', password: 'ShinyNose101' },
-  { email: 'frosty@snowman.com', username: 'FrostyTheSnowman', password: 'WinterWonder202' }
-]
-
-users = users_data.map do |user_attrs|
-  User.create!(user_attrs)
-end
+# Create SantaClaus user
+santa = User.create!(email: 'santa@northpole.com', username: 'SantaClaus', password: 'HoHoHo123')
 
 # Tags
 tags = {
@@ -25,20 +17,20 @@ tags = {
 
 # Flashcards
 physics_flashcards = [
-  { title: 'Newton\'s First Law', front_content: 'State Newton\'s First Law of Motion', back_content: 'An object in motion stays in motion', user: users.sample, tags: [tags[:physics]] },
-  { title: 'Speed of Light', front_content: 'What is the speed of light?', back_content: '299,792,458 meters per second', user: users.sample, tags: [tags[:physics]] }
+  { title: 'Newton\'s First Law', front_content: 'State Newton\'s First Law of Motion', back_content: 'An object in motion stays in motion', user: santa, tags: [tags[:physics]] },
+  { title: 'Speed of Light', front_content: 'What is the speed of light?', back_content: '299,792,458 meters per second', user: santa, tags: [tags[:physics]] }
 ]
 
 rome_flashcards = [
-  { title: 'Roman Colosseum', front_content: 'What was the Roman Colosseum used for?', back_content: 'Public spectacles, gladiator contests', user: users.sample, tags: [tags[:rome]] }
+  { title: 'Roman Colosseum', front_content: 'What was the Roman Colosseum used for?', back_content: 'Public spectacles, gladiator contests', user: santa, tags: [tags[:rome]] }
 ]
 
 javascript_flashcards = [
-  { title: 'JavaScript Usage', front_content: 'What is JavaScript commonly used for?', back_content: 'Web development', user: users.sample, tags: [tags[:javascript]] }
+  { title: 'JavaScript Usage', front_content: 'What is JavaScript commonly used for?', back_content: 'Web development', user: santa, tags: [tags[:javascript]] }
 ]
 
 holiday_flashcards = [
-  { title: 'Santa\'s Reindeers', front_content: 'Name Santa\'s most famous reindeer', back_content: 'Rudolph', user: users.sample, tags: [tags[:holiday]] }
+  { title: 'Santa\'s Reindeers', front_content: 'Name Santa\'s most famous reindeer', back_content: 'Rudolph', user: santa, tags: [tags[:holiday]] }
 ]
 
 # Creating flashcards
